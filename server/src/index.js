59f17f0ce56eacd3511788f07043fe0eca8681fc
Jidnesh Meher <meherjidnesh89@gmail.com>
2025-08-21 00:02:36 +0530

    Package added

--- server/src/index.js
+++ server/src/index.js
@@ -1,7 +1,7 @@
 import {app} from "./app.js"
 import mongoose from "mongoose"
 
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 8000
 
 mongoose.connect(process.env.MONGODB_URI)
 .then(() => {
