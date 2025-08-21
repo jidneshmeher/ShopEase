ed7c86d2e7a781b4fc827fbe07a1884e035ccfb9
Jidnesh Meher <meherjidnesh89@gmail.com>
2025-08-21 23:48:31 +0530

    fix: restore typewriter effect by uncommenting code

--- client/src/features/home/components/Typewriter.jsx
+++ client/src/features/home/components/Typewriter.jsx
@@ -1,30 +1,30 @@
 import { useEffect, useState } from 'react';
 
 const Typewriter = ({ words, typingSpeed = 150, pauseTime = 2000 }) => {
-  // const [currentWordIndex, setCurrentWordIndex] = useState(0);
-  const [displayText, setDisplayText] = useState(words[2]);
-  // const [typing, setTyping] = useState(true);
+  const [currentWordIndex, setCurrentWordIndex] = useState(0);
+  const [displayText, setDisplayText] = useState('');
+  const [typing, setTyping] = useState(true);
 
-  // useEffect(() => {
-  //   let timeout;
-  //   if (typing) {
-  //     if (displayText.length < words[currentWordIndex].length) {
-  //       timeout = setTimeout(() => {
-  //         setDisplayText((prev) => prev + words[currentWordIndex][prev.length]);
-  //       }, typingSpeed);
-  //     } else {
-  //       timeout = setTimeout(() => setTyping(false), pauseTime);
-  //     }
-  //   } else {
-  //     timeout = setTimeout(() => {
-  //       setTyping(true);
-  //       setDisplayText('');
-  //       setCurrentWordIndex((prev) => (prev + 1) % words.length);
-  //     }, 500);
-  //   }
+  useEffect(() => {
+    let timeout;
+    if (typing) {
+      if (displayText.length < words[currentWordIndex].length) {
+        timeout = setTimeout(() => {
+          setDisplayText((prev) => prev + words[currentWordIndex][prev.length]);
+        }, typingSpeed);
+      } else {
+        timeout = setTimeout(() => setTyping(false), pauseTime);
+      }
+    } else {
+      timeout = setTimeout(() => {
+        setTyping(true);
+        setDisplayText('');
+        setCurrentWordIndex((prev) => (prev + 1) % words.length);
+      }, 500);
+    }
 
-  //   return () => clearTimeout(timeout);
-  // }, [displayText, typing, currentWordIndex, words, typingSpeed, pauseTime]);
+    return () => clearTimeout(timeout);
+  }, [displayText, typing, currentWordIndex, words, typingSpeed, pauseTime]);
 
   return (
     <span className="text-white font-playfair border-r-2 border-white pr-1 animate-pulse">
